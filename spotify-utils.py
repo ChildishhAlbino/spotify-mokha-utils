2cbc02cc3d3cb2b4cb9ef1fd8c5183d93f183dd2
Connor <connorwilliams.home@gmail.com>
2020-05-05 17:43:16 +1000

    fix(auth-token): Change bearer to basic.

--- spotify-utils.py
+++ spotify-utils.py
@@ -9,7 +9,7 @@ def auth(clientID=environ["MOKHA_SPOTIFY_CLIENT_ID"], clientSecret=environ["MOKH
     client_ID_Secret = "%s:%s" % (clientID, clientSecret)
     b64 = base64.b64encode(client_ID_Secret.encode()).decode()
     print(b64)
-    requestHeaders = {"Authorization": "Bearer %s" % (b64)}
+    requestHeaders = {"Authorization": "Basic %s" % (b64)}
     requestBody = {
         "grant_type": "client_credentials"
     }
