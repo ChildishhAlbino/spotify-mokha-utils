c6d82940089dbb84352de8fb512886ce6f0f7c94
Connor <connorwilliams.home@gmail.com>
2020-05-05 17:16:08 +1000

    feature(auth-code): Add first stage of Auth code.

--- spotify-utils.py
+++ spotify-utils.py
@@ -1,3 +1,11 @@
+import requests
+import base64
+from os import environ
 
-def auth():
-    print("Authorizing")
+
+def auth(clientID=environ["MOKHA_SPOTIFY_CLIENT_ID"], clientSecret=environ["MOKHA_SPOTIFY_SECRET_ID"]):
+    client_ID_Secret = "%s:%s" % (clientID, clientSecret)
+    b64 = base64.b64encode(client_ID_Secret.encode()).decode()
+    print(b64)
+    headers = {"Authorization": "Bearer %s" % (b64)}
+    print(headers)
