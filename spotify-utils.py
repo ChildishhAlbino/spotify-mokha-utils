77e11ac88f28300dbcf7a7e9d1f93a8f34ce9ad3
Connor <connorwilliams.home@gmail.com>
2020-05-05 17:41:54 +1000

    chore(auth): Add 2nd Part of Auth functionality.

--- spotify-utils.py
+++ spotify-utils.py
@@ -2,13 +2,19 @@ import requests
 import base64
 from os import environ
 
+tokenUrl = "https://accounts.spotify.com/api/token"
+
 
 def auth(clientID=environ["MOKHA_SPOTIFY_CLIENT_ID"], clientSecret=environ["MOKHA_SPOTIFY_SECRET_ID"]):
     client_ID_Secret = "%s:%s" % (clientID, clientSecret)
     b64 = base64.b64encode(client_ID_Secret.encode()).decode()
     print(b64)
-    headers = {"Authorization": "Bearer %s" % (b64)}
-    print(headers)
+    requestHeaders = {"Authorization": "Bearer %s" % (b64)}
+    requestBody = {
+        "grant_type": "client_credentials"
+    }
+    r = requests.post(tokenUrl, data=requestBody, headers=requestHeaders)
+    print(r.json())
 
 
 def getCurrentPlayingTrack():
